5f4b0319b6dd74bcc8f52fd2d1d5b00741fdcf3f
Celina Peralta <celinanperalta@gmail.com>
2021-04-01 17:22:03 -0400

    frame

---
+++ Spinder/data/schemas.js
@@ -0,0 +1,55 @@
+let user = {
+    "_id" : "60662c937aca080f54785a85",
+    "firstName" : "Bob",
+    "lastName": "Smith",
+    "username": "bsmith",
+    "email" : "bobsmith@gmail.com",
+    "location": {"country": "U.S.", "city": "New York"},
+    "img" : "https://fbcdn-profile-a.akamaihd.net/hprofile-ak-frc3/t1.0-1/1970403_10152215092574354_1798272330_n.jpg",
+    "topArtist": ["artist1", "artist2", "artist3"],
+    "topSongs" : ["song1", "song2", "song3"],
+    "playlists" : ["playlist1", "playlist2", "playlist3"],
+    "likedProfiles" : ["80662c937aca080f54785a83", "70662c937aca080f54785a82", "60663868c70d3c9512ae492e"],
+    "musicalProfile" : "77992c937bca080f54785a55"
+};
+
+let musicalProfile = {
+    "_id" : "77992c937bca080f54785a55",
+    "user_id": "60662c937aca080f54785a85",
+    "topGenres" : ["rock", "vapor wave", "house"],
+    "averageAudioFeatures" : { 
+            "danceability": 0.735,
+            "energy": 0.578,
+            "key": 5,
+            "loudness": -11.84,
+            "mode": 0,
+            "speechiness": 0.0461,
+            "acousticness": 0.514,
+            "instrumentalness": 0.0902,
+            "liveness": 0.159,
+            "valence": 0.624,
+            "tempo": 98.002,
+        }  
+  };
+
+let song = {
+    "_id" : "6066381ac70d3c9512ae492d",
+    "user_ids": [],
+    "spotify_id":,
+    "spotify_url" : [],
+    "name" : ,
+    "artists" : ,
+    "img" : "https://i.scdn.co/image/966ade7a8c43b72faa53822b74a899c675aaafee",
+    "audio_features" :
+  };
+
+let artist = {
+    "_id" : "60662c937aca080f54785a85",
+    "user_ids" : [],
+    "spotify_id":,
+    "spotify_url" : [],
+    "name" : ,
+    "artists" : ,
+    "img" : "https://i.scdn.co/image/32bd9707b42a2c081482ec9cd3ffa8879f659f95" ,
+    "audio_features" :
+  };
