5b3a748c7be764f1da25616185f095d645854bc5
Celina Peralta <celinanperalta@gmail.com>
2021-04-13 23:17:59 -0400

    workflows + views

--- Spinder/routes/index.js
+++ Spinder/routes/index.js
@@ -9,6 +9,10 @@ const constructorMethod = (app) => {
   app.use('/artists', artistRoutes);
   app.use('/songs', songRoutes);
 
+  app.get("/", async (req, res) => {
+    res.render('login', {title: 'Log in to Spinder'})
+  });
+
   app.use('*', (req, res) => {
     res.sendStatus(404);
   });
